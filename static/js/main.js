f3e160e1c8bf93383e9cce1ed3d475e8590ef4dc
d.kapsamun <d.kapsamun@qiwi.com>
2017-11-24 14:01:34 +0300

    Static files.

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,12 @@
+const LocalStorageKey = "242617/torture:address"
 
-
-let address = () => document.getElementById("address").value;
+let address = () => document.getElementById("addressValue").value;
 
 $(() => {
+
+	document.getElementById("addressBtn").addEventListener("click", () => {
+		window.localStorage.setItem(LocalStorageKey, document.getElementById("addressValue").value)
+	});
 
 	let enabled = document.getElementById("enabled");
 	enabled.addEventListener("change", event => {
@@ -10,12 +14,8 @@ $(() => {
 		.then(console.log)
 		.catch(console.error);
 	});
-	new Switchery(enabled);
-
-
 
 	let slider  = $("#slider");
-
 	slider.slider({
 		animate: true,
 		range: "min",
@@ -24,29 +24,40 @@ $(() => {
 		step: 10,
 
 		slide: (event, ui) => {
+			let value  = slider.slider("value"),
 
-			let value  = slider.slider('value'),
-
-			volume = $('.volume');
+			volume = $(".volume");
 			if (value <= 5) { 
-				volume.css('background-position', '0 0');
+				volume.css("background-position", "0 0");
 			} else if (value <= 25) {
-				volume.css('background-position', '0 -25px');
+				volume.css("background-position", "0 -25px");
 			} else if (value <= 75) {
-				volume.css('background-position', '0 -50px');
+				volume.css("background-position", "0 -50px");
 			} else {
-				volume.css('background-position', '0 -75px');
+				volume.css("background-position", "0 -75px");
 			};
 
 			fetch(address(), { method: "PUT", body: JSON.stringify({ volume: value }) })
 			.then(console.log)
 			.catch(console.error);
-
 		},
 		// start: (event, ui) => {},
 		// stop: (event, ui) => {},
-
 	});
 
+	fetch(address(), { method: "GET" })
+	.then(response => response.json())
+	.then(status => {
+		console.log("status.enabled", status.enabled)
+		enabled.checked = status.enabled ? "on" : "off";
+
+		console.log("status.volume", status.volume)
+		slider.slider("value", status.volume);
+
+		new Switchery(document.getElementById("enabled"))
+	})
+	.catch(console.error);
+
+	document.getElementById("addressValue").value = window.localStorage.getItem(LocalStorageKey)
 
 });
